a5f421dde1dbb4b67696696417f3eccf5f4a0e0a
BardinPetr <bardin.petr@gmail.com>
2016-12-10 19:21:48 +0300

    Add: speed modes; Fixed: indication and send

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -20,16 +20,16 @@
 
 bool sflag = false;
 
-SoftwareSerial sr(3,2);
-
+//SoftwareSerial sr(3, 2);
+int speedmode = 0;
 CatLink link(0x22);
 
 int BTNID() {
   if (!digitalRead(BTN1)) return 1;
   else if (!digitalRead(BTN2)) return 2;
   else if (!digitalRead(BTN3)) return 3;
-  else if (!digitalRead(BTN4)) return 4;
-  else if (!digitalRead(BTNST)) return 5;
+  else if (!digitalRead(BTN4)){speedmode = (speedmode == 2 ? 1:2); return 4;}
+  else if (!digitalRead(BTNST)) {speedmode = (speedmode == 1 ? 0:1); return 5;}
   else if (analogRead(MSTICK_GOR) > 800) return 6;
   else if (analogRead(MSTICK_GOR) < 200) return 7;
   else if (analogRead(MSTICK_VERT) > 800) return 8;
@@ -44,39 +44,42 @@ void setup()
   pinMode(BTN2, INPUT_PULLUP);
   pinMode(BTN3, INPUT_PULLUP);
   pinMode(BTN4, INPUT_PULLUP);
-  pinMode(BTNST, INPUT_PULLUP);  
-  
+  pinMode(BTNST, INPUT_PULLUP);
+
   pinMode(LEDA, OUTPUT);
   pinMode(LEDB, OUTPUT);
   pinMode(LEDC, OUTPUT);
   pinMode(LEDD, OUTPUT);
 
   Serial.begin(9600);
-  
+
   link.bind(1, RecData);
 }
 
-void loop()
+void loop() 
 {
   int state = BTNID();
   
-  if (link.st1(150)) {/*
-    if (state > 0 && state < 6) {
-      link.Send(3, state, 0);
-    }
-    else if (state > 5 && state < 10) {
-      link.Send(2, state, 0);
-    }
-    else {*/
-      DriveSend();
-    //}
+  if (state > 0 && state < 6) {
+    link.Send(2, state, 0);
+    delay(300);
+  }
+  else if (state > 5 && state < 10) {
+    link.Send(3, state, 0);
   }
   
-  digitalWrite(LEDB, link.online);
+  else {
+    if (link.st1(100)) {
+      DriveSend();
+    }
+  }
+  
+  digitalWrite(LEDA, (speedmode == 2 ? 1:0));
+  digitalWrite(LEDB, (speedmode == 1 ? 1:0));
 }
 
 void RecData(byte i1, byte i2) {
-
+  //digitalWrite(LEDB, link.online);
 }
 
 void DriveSend()
@@ -102,22 +105,22 @@ void DriveSend()
 
   motor1 = map(motor1, -255, 255, 0, 255);
   motor2 = map(motor2, -255, 255, 0, 255);
-  
+
   if (motor1 > 255)   motor1 = 255;
   if (motor2 > 255)   motor2 = 255;
   if (motor1 < 0)  motor1 = 0;
   if (motor2 < 0)  motor2 = 0;
 
-  if(motor2 < (motor1 + 10) && motor2 > (motor1 - 10)) motor2 = motor1;
+  if (motor2 < (motor1 + 10) && motor2 > (motor1 - 10)) motor2 = motor1;
 
-//  Serial.print((int)((motor1 - 127) * 2));
-//  Serial.print(" ");
-//  Serial.print((int)((motor2 - 127) * 2));  
-//  Serial.print("    ");
-//
-//  Serial.print(motor1);
-//  Serial.print(" ");
-//  Serial.println(motor2);
-  
+  //  Serial.print((int)((motor1 - 127) * 2));
+  //  Serial.print(" ");
+  //  Serial.print((int)((motor2 - 127) * 2));
+  //  Serial.print("    ");
+  //
+  //  Serial.print(motor1);
+  //  Serial.print(" ");
+  //  Serial.println(motor2);
+
   link.Send(1, motor1, motor2);
 }
