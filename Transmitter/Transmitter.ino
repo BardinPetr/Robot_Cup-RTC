aaf0da22585efb205407e733a05fe9cc0d1e0f36
BardinPetr <bardin.petr@gmail.com>
2016-12-13 15:41:28 +0300

    Speed controll system changes (now you can use only functional joystick button to switch between modes)

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -19,19 +19,19 @@
 #define MSTICK_GOR       A1
 
 bool sflag = false;
-int speedmode = 0;
+int speedmode = 1;
 
 SoftwareSerial sr(3, 2);
 CatLink link(0x22, sr);
 
 int BTNID() {
   if (!digitalRead(BTN1)) return 1;
-  else if (!digitalRead(BTN2)) return 2;
-  else if (!digitalRead(BTN3)) return 3;
-  else if (!digitalRead(BTN4)){speedmode = (speedmode == 2 ? 1:2); return 4;}
-  else if (!digitalRead(BTNST)) {speedmode = (speedmode == 1 ? 0:1); return 5;}
-  else if (analogRead(MSTICK_GOR) > 800) return 6;
-  else if (analogRead(MSTICK_GOR) < 200) return 7;
+  else if (!digitalRead(BTN2))  return 2;
+  else if (!digitalRead(BTN3))  return 3;
+  else if (!digitalRead(BTN4))  return 4;
+  else if (!digitalRead(BTNST)) return 5;
+  else if (analogRead(MSTICK_GOR) > 800)  return 6;
+  else if (analogRead(MSTICK_GOR) < 200)  return 7;
   else if (analogRead(MSTICK_VERT) > 800) return 8;
   else if (analogRead(MSTICK_VERT) < 200) return 9;
   else return 0;
@@ -62,6 +62,7 @@ void loop()
   
   if (state > 0 && state < 6) {
     link.Send(2, state, 0);
+    if(state == 5) speedmode = (speedmode == 0 ? 1:(speedmode == 1 ? 2:(speedmode == 2 ? 0:0)));
     delay(300);
   }
   else if (state > 5 && state < 10) {
@@ -74,9 +75,8 @@ void loop()
       DriveSend();
     }
   }
-  
   digitalWrite(LEDA, (speedmode == 2 ? 1:0));
-  digitalWrite(LEDB, (speedmode == 1 ? 1:0));
+  digitalWrite(LEDC, (speedmode == 1 ? 1:0));
 }
 
 void RecData(byte i1, byte i2) {
@@ -92,9 +92,6 @@ void DriveSend()
 
   vert_stick = map(vert_stick, 0, 1023, -255, 255);
   gor_stick = map(gor_stick, 0, 1023, -255, 255);
-
-  //if (vert_stick > -5 && vert_stick < 5)vert_stick = 0;
-  //if (gor_stick > -5 && gor_stick < 5)gor_stick = 0;
 
   motor1 = vert_stick - gor_stick;
   motor2 = vert_stick + gor_stick;
